9fd891a586cef457ac574a47a37305b10c39e3cb
Никита <nikita@MacBook-Air-Olesa.local>
2026-09-18 04:18:47 +0300

    lpc-uploader

--- Casks/lpc-uploader.rb
+++ Casks/lpc-uploader.rb
@@ -2,8 +2,8 @@ cask "lpc-uploader" do
   arch arm: "arm64", intel: "x64"
 
   version "1.0.3"
-  sha256 arm:   "",
-         intel: ""
+  sha256 arm:   "03b7c68a98444acc385677532cbb6723bc2a5fa94b5e7f85fcd8180c0b435e43",
+         intel: "31910f9c39aa40c37b1323a696dbc4d5407182d861fb346e2313b865ab29f53e"
 
   url "https://github.com/NikitaBatryncha/lpc-uploader-releases/releases/download/v#{version}/LPC-Uploader-#{version}-#{arch}.zip"
   name "LPC Uploader"
