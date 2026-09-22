7cb9d914db74a1d2e447d682cac5a4aacc94ddff
Никита <nikita@MacBook-Air-Olesa.local>
2026-09-22 14:59:41 +0300

    lpc-uploader 1.1.1

--- Casks/lpc-uploader.rb
+++ Casks/lpc-uploader.rb
@@ -5,7 +5,7 @@ cask "lpc-uploader" do
   sha256 arm:   "820681112998d93912fdd5793ef18ba8155610c6bc384b2749a4fe1d6eacbbab",
          intel: "5a3cf5e7f306f630e16cee4e35716cec8e0f4cdfc1f7fd9a6ec2d8f72bd7de01"
 
-  url "https://github.com/NikitaBatryncha/lpc-uploader-releases/releases/download/v#{version}/lpc-uploader-#{version}-#{arch}.zip"
+  url "https://github.com/NikitaBatryncha/lpc-uploader-releases/releases/download/v#{version}/lpc-uploader-#{version}-#{arch}.dmg"
   name "LPC Uploader"
   desc "Фоновый сервер для загрузки ассетов из Figma-плагина"
   homepage "https://github.com/NikitaBatryncha/lpc-uploader-releases"
